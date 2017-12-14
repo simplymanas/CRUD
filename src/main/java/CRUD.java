5c04079372a8c0b1c5cd42fab44792a424bd2542
ga15 <manas.dash@in.tesco.com>
2017-12-14 06:37:00 +0530

    adding CRUD interface

---
+++ src/main/java/CRUD.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Manas Dash on 11/12/17 8:01 AM.
+ * 100DaysOFCode
+ */
+public interface CRUD {
+
+    int Create(Object object);
+    Object Read(Object object);
+    int Update(Object object);
+    int Delete(Object object);
+}
